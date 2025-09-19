16dc7d4c7c46006c3a72b86892283aba8e564271
Arman <mohammadjahedulislamarman@gmail.com>
2025-09-20 00:11:32 +0600

    complete the react world project

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -6,9 +6,17 @@ import './Countries.css';
 const Countries = ({ countriesPromise }) => {
 
     const [visitedCountries, setVisitedCountries] = useState([]);
+    const [visitedFlags, setVisitedFlags] = useState([])
 
     const handleVisitedCountries = (country) => {
         console.log("handle visited country clicked", country)
+        const newVisitedCountries = [...visitedCountries, country]
+        setVisitedCountries(newVisitedCountries)
+    }
+
+    const handleVisitedFlags = (flag) => {
+        const newVisitedFlags = [...visitedFlags, flag]
+        setVisitedFlags(newVisitedFlags)
     }
 
     const countriesData = use(countriesPromise);
@@ -17,13 +25,31 @@ const Countries = ({ countriesPromise }) => {
     return (
         <div>
             <h1> In the Country : {countries.length} </h1>
-            <h3>Total Country Visited : </h3>
+            <h3>Total Country Visited : {visitedCountries.length}</h3>
+            <h3>Total Flag visited : {visitedFlags.length}</h3>
+            <ol>
+                {
+                    visitedCountries.map(country =>
+                        <li
+                            key={country.cca3.cca3}
+                        >{country.name.common}</li>
+                    )
+                }
+            </ol>
+
+            <div className='visited-flags-container'>
+                {
+                    visitedFlags.map((flag, index) => <img key={index} src={flag}></img>)
+                }
+            </div>
+
             <div className='countries'>
                 {
                     countries.map(country => <Country
                         key={country.cca3.cca3}
                         country={country}
                         handleVisitedCountries={handleVisitedCountries}
+                        handleVisitedFlags={handleVisitedFlags}
                     ></Country>)
                 }
             </div>
